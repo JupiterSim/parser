a6e30052178fa5ab59496d757d90ea048fe82057
Andrés Castellanos <andres.cv@galileo.edu>
2020-06-14 15:07:55 -0600

    feat: add symbol table type

---
+++ src/table.ts
@@ -0,0 +1,42 @@
+import { Segment } from './segment';
+import { Expression } from './expr';
+import { DebugInfo } from './debug';
+
+/**
+ * Assembler label.
+ */
+export interface Label {
+  /** Symbol segment. */
+  readonly segmnet: Segment;
+  /** Symbol debug information. */
+  readonly debugInfo: DebugInfo;
+  /** Symbol address. */
+  address: number;
+}
+
+/**
+ * Assembler symbol table.
+ */
+export interface SymbolTable {
+  /** Globals symbols. */
+  readonly globals: {
+    /** Symbol name. */
+    [symbol: string]: Label;
+  };
+  /** Local symbols. */
+  readonly locals: {
+    /** File name where the local symbol is. */
+    [filename: string]: {
+      /** Symbol name. */
+      [symbol: string]: Label;
+    };
+  };
+  /** Local expressions. */
+  readonly expr: {
+    /** File name where the local symbol is. */
+    [filename: string]: {
+      /** Symbol name. */
+      [symbol: string]: Expression;
+    };
+  };
+}
