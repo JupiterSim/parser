ac35c9a9e7ba5337070f73650ceee1d090b4f33d
Andrés Castellanos <andres.cv@galileo.edu>
2020-06-19 22:04:44 -0600

    test: add label listener tests

---
+++ test/listeners/label.test.ts
@@ -0,0 +1,22 @@
+import chai from 'chai';
+import { Test } from './utils';
+import { Label } from '../../src/formats';
+
+describe('Labels', () => {
+  class LblTest extends Test {
+    constructor() {
+      super('foo: add a0, a0, a0');
+    }
+
+    label = (ctx: Label): void => {
+      it('label should be foo', () => {
+        chai.expect(ctx.name).to.eq('foo');
+      });
+    };
+  }
+
+  const empty = new Test('label: add a0, a0, a0');
+  const test = new LblTest();
+  test.parse();
+  empty.parse();
+});
