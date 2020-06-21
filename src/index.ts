bc8966e927fc54fd57a09203579e9200048ac4b3
Andrés Castellanos <andres.cv@galileo.edu>
2020-06-21 12:33:11 -0600

    refactor: remove old code to create handwritten lexer and parser for performance

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './parser';
-export * from './relocation';
-export * from './segment';
-export * from './types';
--- src/syntax/index.ts
+++
@@ -1,3 +0,0 @@
-export { RISCVLexer } from './RISCVLexer';
-export { RISCVParser } from './RISCVParser';
-export { RISCVListener } from './RISCVListener';
