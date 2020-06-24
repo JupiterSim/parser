f6c0ef88852e18684a236135443755aada0dfc24
Andrés Castellanos <andres.cv@galileo.edu>
2020-06-23 20:45:05 -0600

    test: add tests for label longest match

--- test/lexer/ids.test.ts
+++ test/lexer/ids.test.ts
@@ -7,10 +7,10 @@ describe('Lexer Ids', () => {
   const data = fs.readFileSync(path.resolve(__dirname, 'data', 'ids.tokens'), { encoding: 'utf-8' });
   const lexer = new Lexer(data);
   const tokens = lexer.tokens();
-  it('should generate 9 tokens', () => {
-    chai.expect(tokens.length).to.eq(9);
+  it('should generate 10 tokens', () => {
+    chai.expect(tokens.length).to.eq(10);
   });
-  const expect = [IDS.LABEL, IDS.LABEL, IDS.LABEL, IDS.ID, IDS.ID, IDS.ID, IDS.ID, IDS.EOL, IDS.EOF];
+  const expect = [IDS.LABEL, IDS.LABEL, IDS.LABEL, IDS.ID, IDS.ID, IDS.LABEL, IDS.ID, IDS.ID, IDS.EOL, IDS.EOF];
   it('should match all expected tokens', () => {
     tokens.forEach((e, i) => {
       chai.expect(e.id).to.eq(expect[i]);
